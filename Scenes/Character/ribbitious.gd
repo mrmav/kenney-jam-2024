f7599401e544ba1bac46ef3ff6770414f346f7ae
Torres-Git <luismigueltorres@outlook.pt>
2024-07-20 21:25:54 +0100

    Added clouds  idle animation

--- Scenes/Character/ribbitious.gd
+++ Scenes/Character/ribbitious.gd
@@ -1,37 +1,33 @@
 extends Control
 
 export var button_path: NodePath
-export var lerp_factor: float = 0.9
-
+export var clouds_path: NodePath
 var button: Button
+var cloud: Control
 
 var start_offset = Vector2(0,100)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	button = get_node(button_path) as Button
+	cloud = get_node(clouds_path) as Control
 	
 	button.connect("mouse_entered", self, "_on_Area2D_mouse_entered")
 	button.connect("mouse_exited", self, "_on_Area2D_mouse_exited")
 	
 	rect_position = rect_position + start_offset
-	
 	pass # Replace with function body.
 
 
 	
 func _on_Area2D_mouse_entered() -> void:
 	print("Hello ")
-	var target_pos = rect_position - start_offset
-	rect_position = target_pos
-
-	# Increase the size of the sprite
+	rect_position = rect_position - start_offset
+	cloud.visible = false
 	pass
-	
 
-	
 func _on_Area2D_mouse_exited() -> void:
 	print("Bye ")
 	# Return to the default size
-	var target_pos = rect_position + start_offset
-	rect_position = target_pos
+	rect_position = rect_position + start_offset
+	cloud.visible = true
 	pass
