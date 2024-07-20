b1480f84edd5dff94a0614b6044b2df1f186ae55
mrmav <mrmav@outlook.pt>
2024-07-20 10:22:06 +0100

    Configure server (#6)
    * Added simple server reply
    
    * test get and post
    
    * sample tree struct

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+var bodyParser = require('body-parser')
+var mysql = require('mysql');
+const app = express();
+const port = 3000;
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+ 
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.get('/', (req, res) => {
+  var date = new Date();
+  res.send('Hello World! This is a reply fromn the webserver!\n Request Date: ' + date);
+});
+
+app.post('/post-data', jsonParser, (req, res) => {
+  const data = req.body;
+
+  // Save the data of user that was sent by the client
+  console.log("Received data:", data)
+
+  // Send a response to client that will show that the request was successfull.
+  res.send({
+    message: 'Data delivered!',
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
