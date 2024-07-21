b6e60d8b497fda062193079943b7e4d763b94ce4
mrmav <mrmav@outlook.pt>
2024-07-21 01:33:51 +0100

    Node editor line (#15)
    * Camera zoom on initial node (root);
    Setup editor node connections;
    
    * Adde selected animation;
    Added already selected validation;
    
    * fixed root node not being selected;
    wrote some shady code to zoom camera with scroll;
    adde a way to edit the tree_nodes whithout ffecting th echildren
    
    * adjusted some positions of the nodes

--- Scenes/NodeContainer/GameCamera.gd
+++ Scenes/NodeContainer/GameCamera.gd
@@ -1,15 +1,40 @@
 extends Camera2D
 
+export(Vector2) var zoom_min_max = Vector2(1, 3)
+export(float) var zoom_amount = 0.1
 
 func _ready():
 	GlobalAccess.node_container.connect("node_selected", self, "_on_node_selected")
 	
 	yield(GlobalAccess.node_container, "ready")
-	smoothing_enabled = false
 	_on_node_selected(GlobalAccess.node_container.selected_node)
-	smoothing_enabled = true
 
-
+var tween = null
+func _process(delta):
+	
+	if tween and tween.is_running():
+		print("tweeen trunning ")
+		return
+	
+	var new_zoom = zoom.x
+	
+	if Input.is_action_just_released("zoom_in"):
+		new_zoom -= zoom_amount
+		
+	if Input.is_action_just_released("zoom_out"):
+		new_zoom += zoom_amount
+	
+	if new_zoom != zoom.x:		
+		var a = clamp(new_zoom, zoom_min_max.x, zoom_min_max.y)
+		
+		tween = get_tree().create_tween()
+		tween.set_ease(Tween.EASE_OUT)
+		tween.set_trans(Tween.TRANS_ELASTIC)
+		tween.tween_property(self, "zoom", Vector2(a, a), 0.6)
+		tween.play()
+		print("playe twween ")
+	
+	
 func _on_node_selected(node : TreeNode):
 	print(node.global_position)
 	global_position = node.global_position
