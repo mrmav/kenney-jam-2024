ae4ad4f844601fed3bf11ad69c38baa6b8e9b252
mrmav <mrmav@outlook.pt>
2024-07-21 12:22:47 +0100

    added check recipe to server

--- server/index.js
+++ server/index.js
@@ -13,9 +13,29 @@ var jsonParser = bodyParser.json()
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
+
+var good_recipe = [10, 5, 7]
+
+
 app.get('/', (req, res) => {
   var date = new Date();
   res.send('Hello World! This is a reply fromn the webserver!\n Request Date: ' + date);
+});
+
+app.get('/check', (req, res) => {
+  var date = new Date();
+  console.log(date, req.query.recipe);
+  
+  var buff = Buffer.from(req.query.recipe, 'base64');
+  
+  var player_array = JSON.parse(buff.toString());
+  
+
+  var response = {
+    "result": calculateCorrectPositionPercentile(good_recipe, player_array)
+  }
+
+  res.send(JSON.stringify(response));
 });
 
 app.post('/post-data', jsonParser, (req, res) => {
@@ -32,4 +52,36 @@ app.post('/post-data', jsonParser, (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+
+  console.log(calculateCorrectPositionPercentile(good_recipe, [10,  5,  7]));
+  console.log(calculateCorrectPositionPercentile(good_recipe, [10,  5]));
+  console.log(calculateCorrectPositionPercentile(good_recipe, [10]));
+  console.log(calculateCorrectPositionPercentile(good_recipe, [10,  89,  7]));
+  console.log(calculateCorrectPositionPercentile(good_recipe, [10, 89,  7,  5]));
+  console.log(calculateCorrectPositionPercentile(good_recipe, [10, 89,  7,  5, 10]));
+  console.log(calculateCorrectPositionPercentile(good_recipe, [10, 89,  7,  5, 10, 2, 4,7,7]));
+
 });
+
+
+// by chatgpt
+function calculateCorrectPositionPercentile(goodArray, testArray) {
+  // Determine the length of the shorter and longer arrays
+  const minLength = Math.min(goodArray.length, testArray.length);
+  const maxLength = Math.max(goodArray.length, testArray.length);
+
+  // Count the number of elements that are in the correct index position
+  let correctCount = 0;
+  for (let i = 0; i < minLength; i++) {
+      if (goodArray[i] === testArray[i]) {
+          correctCount++;
+      }
+  }
+
+  // Calculate the total number of comparisons (including the extra elements)
+  const totalComparisons = maxLength;
+
+  // Calculate the adjusted percentile
+  const percentile = (correctCount / totalComparisons) * 100;
+  return percentile;
+}
