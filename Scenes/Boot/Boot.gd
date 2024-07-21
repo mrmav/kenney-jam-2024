4f3379d406d2219c54a84b5c48c916f414f06120
mrmav <mrmav@outlook.pt>
2024-07-21 09:05:03 +0100

    Made better camera dinamics that should make mobile feel more included;

--- Scenes/Boot/Boot.gd
+++ Scenes/Boot/Boot.gd
@@ -4,6 +4,11 @@ export(PackedScene) var change_to_this_scene = null
 
 func _ready():
 	
+	if not OS.has_feature("HTML5"):
+		# if not running on the web, skip direclty
+		get_tree().change_scene_to(change_to_this_scene)
+		return
+	
 	var t = get_tree().create_timer(5)
 	yield(t, "timeout")
 	
