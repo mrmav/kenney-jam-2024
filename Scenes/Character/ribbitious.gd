e19005d732b8fa9623d4a14407820f622dc8f53f
mrmav <mrmav@outlook.pt>
2024-07-27 08:22:52 +0100

    Added a delay in the character reveal animation

--- Scenes/Character/ribbitious.gd
+++ Scenes/Character/ribbitious.gd
@@ -7,6 +7,8 @@ export var background_animation_path: NodePath
 export var char_animation_player_path: NodePath
 export var dismiss_btn_nodepath: NodePath
 
+export(float) var needed_hover_time_sec = 1.0
+
 var hover_button: Button
 var submit_text: Control
 var cloud: Control
@@ -14,11 +16,11 @@ var background_animation: AnimationPlayer
 var char_animation_player: AnimationPlayer
 var dismiss_btn: Button
 
-var start_offset = Vector2(0,100)
-var start_offset_submit = Vector2(0,-150)
-
+var start_offset = Vector2(0, 100)
+var start_offset_submit = Vector2(0, -150)
 
 var submit_open = false
+var _target_over_time: float = -1.0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -26,35 +28,60 @@ func _ready():
 	hover_button = get_node(hover_button_path) as Button
 	submit_text = get_node(submitText_path) as Control
 	cloud = get_node(clouds_path) as Control
-	background_animation =  get_node(background_animation_path) as AnimationPlayer
-	char_animation_player =  get_node(char_animation_player_path) as AnimationPlayer
-	dismiss_btn =  get_node(dismiss_btn_nodepath)
-	
+	background_animation = get_node(background_animation_path) as AnimationPlayer
+	char_animation_player = get_node(char_animation_player_path) as AnimationPlayer
+	dismiss_btn = get_node(dismiss_btn_nodepath)
+
 	hover_button.connect("mouse_entered", self, "_on_HoverButton_mouse_entered")
+	hover_button.connect("mouse_exited", self, "_on_HoverButton_mouse_leave")
 	hover_button.connect("pressed", self, "_on_Button_pressed")
-	
+
 	dismiss_btn.connect("pressed", self, "_dismiss")
-	
-	
+
+
+func _process(_delta):
+	if submit_open or _target_over_time < 0.0:
+		return
+
+	if Time.get_ticks_msec() >= _target_over_time:
+		_open()
+
+
 func _on_HoverButton_mouse_entered() -> void:
 	if submit_open:
 		return
-		
-	print("Hello ")
-	submit_open = true
-	background_animation.play("A_backgroundSwitch_off")
-	char_animation_player.play("reveal")
+
+	_target_over_time = Time.get_ticks_msec() + needed_hover_time_sec * 1000.0
+
+
+func _on_HoverButton_mouse_leave() -> void:
+	if submit_open:
+		return
+
+	_target_over_time = -1.0
 
 
 func _on_Button_pressed() -> void:
 	_dismiss()
 
 
+func _open():
+	print("Hello ")
+	submit_open = true
+	background_animation.play("A_backgroundSwitch_off")
+	char_animation_player.play("reveal")
+
+
 func _dismiss():
 	print("Bye ")
-	submit_open = false
+	_reset()
 	background_animation.play("A_backgroundSwitch_on")
 	char_animation_player.play("reveal", -1, -1, true)
+
+
+func _reset():
+	submit_open = false
+	_target_over_time = -1.0
 
 
 func toggle_thinking_clouds():
